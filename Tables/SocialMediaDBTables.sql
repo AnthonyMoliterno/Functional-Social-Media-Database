aa84f5ddebe995d91c503067f4314fbc43d0f6eb
Anthony Moliterno <Anttyjosh3831@gmail.com>
2025-01-28 14:22:11 -0500

    Added consistent conventional naming throughout tables, constraint to tables, fixed data types, tabe for storing multiple pictures/videos for comments for effiency and scalability,etc

--- Tables/SocialMediaDBTables.sql
+++ Tables/SocialMediaDBTables.sql
@@ -8,18 +8,25 @@ CREATE TABLE UserInformation (
 	CreationDate SMALLDATETIME NOT NULL DEFAULT GETDATE(),
 	PasswordHash VARBINARY(255) NOT NULL,
 	DateOfBirth DATE NOT NULL,
-	Country VARCHAR(56) NOT NULL
+	Country VARCHAR(56) NOT NULL,
+	Bio NVARCHAR(160) NULL,
+	ProfilePictureUrl VARCHAR(255) NULL,
+	CONSTRAINT UQ_UserEmail UNIQUE (EmailAddress)
 );
 
 -- Post *Check any users post information, check specific posts with postid
 CREATE TABLE Posts (
-	PostId INT PRIMARY KEY IDENTITY,
+	PostId INT PRIMARY KEY IDENTITY(1, 1),
 	UserId INT NOT NULL,
-	TextContent NVARCHAR(280),
+	TextContent NVARCHAR(280) NULL,
 	Likes INT DEFAULT 0,
 	Comments INT DEFAULT 0,
+	MediaUrl VARCHAR(255) NULL,
 	PostDate DATE NOT NULL DEFAULT GETDATE(),
-FOREIGN KEY (UserId) REFERENCES UserInformation(UserId)
+	FOREIGN KEY (UserId) REFERENCES UserInformation(UserId) ON DELETE CASCADE,
+
+	-- Ensure A comment has a TextContent or MediaUrl
+	CONSTRAINT CHK_Post_Valid CHECK (TextContent IS NOT NULL OR MediaURL IS NOT NULL)
 );
 
 -- Comments *Checking comments on post, comments made by specific user,
@@ -27,12 +34,31 @@ CREATE TABLE PostComments (
 	CommentId INT PRIMARY KEY IDENTITY,
 	PostId INT NOT NULL,
 	UserId INT NOT NULL,
-	CommentText NVARCHAR(280),
+	CommentText NVARCHAR(280) NULL,
 	CommentDate DATE NOT NULL DEFAULT GETDATE(),
+	MediaUrl VARCHAR(255) NULL,
 	FOREIGN KEY (UserId) REFERENCES UserInformation(UserId),
-	FOREIGN KEY (PostId) REFERENCES Posts(PostId)
+	FOREIGN KEY (PostId) REFERENCES Posts(PostId) ON DELETE CASCADE,
+
+	-- Ensure a comment has either a CommentText or MediaUrl
+	CONSTRAINT CHK_Post_Valid CHECK (CommentText IS NOT NULL OR MediaURL IS NOT NULL)
+);
+
+-- Comments made with multiple images or videos attached 
+CREATE TABLE CommentMedia (
+	MediaId INT PRIMARY KEY IDENTITY,
+	CommentId INT NOT NULL,
+	UserId INT NOT NULL,
+	MediaUrl VARCHAR(255) NOT NULL,
+	MediaType VARCHAR(50) NULL,
+	FOREIGN KEY (CommentId) REFERENCES PostComments(CommentId) ON DELETE CASCADE,
+	FOREIGN KEY (UserId) REFERENCES UserInformation(UserId),
+	-- Ensure media types are valid
+	CONSTRAINT CHK_MediaType CHECK (MediaType IN ('image', 'video', 'audio'))
+
 	);
 
+-- A post likes *trace who liked a post, etc
 CREATE TABLE PostLikes (
 	UserId INT NOT NULL,
 	PostId INT NOT NULL,
