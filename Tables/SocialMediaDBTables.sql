b0e848bb3490af159d4f7c476f17b347d1ac4248
Anthony Moliterno <Anttyjosh3831@gmail.com>
2025-01-27 22:50:33 -0500

    Added critics to functionality, professional table naming, and comments

--- Tables/SocialMediaDBTables.sql
+++ Tables/SocialMediaDBTables.sql
@@ -1,37 +1,44 @@
 USE SocialMediaDataBase;
 
+-- Users information *login, etc
 CREATE TABLE UserInformation (
-	UserId INT PRIMARY KEY,
-	Username VARCHAR(24) NOT NULL,
-	EmailAddress VARCHAR(254) NOT NULL,
-	CreationDate SMALLDATETIME NOT NULL
+	UserId INT PRIMARY KEY IDENTITY(1000000, 1),
+	Username NVARCHAR(24) NOT NULL,
+	EmailAddress NVARCHAR(254) NOT NULL,
+	CreationDate SMALLDATETIME NOT NULL DEFAULT GETDATE(),
+	PasswordHash VARBINARY(255) NOT NULL,
+	DateOfBirth DATE NOT NULL,
+	Country VARCHAR(56) NOT NULL
 );
 
-CREATE TABLE UserPosts (
+-- Post *Check any users post information, check specific posts with postid
+CREATE TABLE Posts (
 	PostId INT PRIMARY KEY IDENTITY,
 	UserId INT NOT NULL,
-	TextContent VARCHAR(280),
+	TextContent NVARCHAR(280),
 	Likes INT DEFAULT 0,
 	Comments INT DEFAULT 0,
-	PostDate DATE NOT NULL,
+	PostDate DATE NOT NULL DEFAULT GETDATE(),
 FOREIGN KEY (UserId) REFERENCES UserInformation(UserId)
 );
 
-CREATE TABLE UsersComments (
+-- Comments *Checking comments on post, comments made by specific user,
+CREATE TABLE PostComments (
 	CommentId INT PRIMARY KEY IDENTITY,
 	PostId INT NOT NULL,
 	UserId INT NOT NULL,
-	CommentText VARCHAR(280),
-	CommentDate DATE NOT NULL,
+	CommentText NVARCHAR(280),
+	CommentDate DATE NOT NULL DEFAULT GETDATE(),
 	FOREIGN KEY (UserId) REFERENCES UserInformation(UserId),
-	FOREIGN KEY (PostId) REFERENCES UserPosts(PostId)
+	FOREIGN KEY (PostId) REFERENCES Posts(PostId)
 	);
 
-CREATE TABLE UsersLikes (
+CREATE TABLE PostLikes (
 	UserId INT NOT NULL,
 	PostId INT NOT NULL,
-	LikedDate DATE NOT NULL,
+	LikedDate DATE NOT NULL DEFAULT GETDATE(),
 	PRIMARY KEY (Userid, Postid),
 	FOREIGN KEY (UserId) REFERENCES UserInformation(UserId),
-	FOREIGN KEY (PostId) REFERENCES UserPosts(PostId)
+	FOREIGN KEY (PostId) REFERENCES Posts(PostId)
 	);
+
